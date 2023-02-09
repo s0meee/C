7ebb3f741a37dbed4afb6d68f4c2d4628a580de8
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-09 04:14:45 +0000

    an integer operation

--- .ccls-cache/@home@runner@C/main.c
+++ .ccls-cache/@home@runner@C/main.c
@@ -1,16 +1,27 @@
-//1.sizeof()연산자 : 변수나 자료형의 크기를 바이트로 반환하는 연산자
-#include<stdio.h>
+//1.정수형 연산
+#include <stdio.h>
 int main()
 {
-  int x;
-  printf("변수 x의 크기 : %d\n", sizeof(x)); //4
-  printf("char형의 크기: %d\n", sizeof(char)); //1
-  printf("int형의 크기 : %d\n", sizeof(x)); //4
-  printf("short형의 크기: %d\n", sizeof(short)); //2
-  printf("long형의 크기 : %d\n", sizeof(x)); //4
-  printf("long long형의 크기 : %d\n", sizeof(long long)); //8
-  printf("float형의 크기 : %d\n", sizeof(float));//4
-  printf("double형의 크기 : %d\n", sizeof(double));//8
+	int x, y, result; //변수 3개 선언
+	printf("두개의 정수를 입력하시오: "); //어떤값을 입력 받을지 안내문 
+	scanf("%d %d", &x, &y); //두개의 정수 차례대로 입력 받을 것 
 
-  return 0;
+	result = x + y; // 더하기 연산자, 피 연산자 X와 y 두개 더한값 result에 저장
+	printf("%d + %d = %d\n", x, y, result); //x+y=result 
+
+  result = x - y; // 빼기 연산자, 피 연산자 X와 y 두개 뺀값 result에 저장
+	printf("%d - %d = %d\n", x, y, result); //x-y=result 
+
+  result = x * y; // 곱하기 연산자, 피 연산자 X와 y 두개 곱한값 result에 저장
+	printf("%d * %d = %d\n", x, y, result); //x*y=result 
+
+  result = x / y;  
+  //정수/정수=정수 나누기 연산자, 피 연산자 X와 y 두개 나눈값 result에 저장
+	printf("%d / %d = %d\n", x, y, result); //x/y=result 
+
+  result = x % y;  //나머지
+	printf("%d % %d = %d\n", x, y, result); //x%y=result 
+
+	return 0;
+
 }
